45d32aeff19bc137d21d5829dfdef5952a011048
andrey.mikhailov <andynik76@yandex.ru>
2022-10-12 22:45:01 +0300

    Gofermart 5

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -19,12 +20,15 @@ func main() {
 	server := new(server)
 	handlers.NewServer(&server.Server)
 
+	fmt.Println("/////////////////", server.Address)
 	go func() {
+		fmt.Println("/////////////////", server.Address)
 		s := &http.Server{
 			Addr:    server.Address,
 			Handler: server.Router}
 
 		if err := s.ListenAndServe(); err != nil {
+			fmt.Println("/////////////////", err)
 			constants.Logger.ErrorLog(err)
 			return
 		}
--- internal/environment/cfgsrv.go
+++ internal/environment/cfgsrv.go
@@ -2,6 +2,7 @@ package environment
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -20,15 +21,18 @@ type ServerConfig struct {
 
 func (sc *ServerConfig) SetConfigServer() {
 
+	fmt.Println("********1")
 	addressPtr := flag.String("a", constants.PortServer, "порт сервера")
 	flag.Parse()
 
+	fmt.Println("********2")
 	var cfgENV ServerConfigENV
 	err := env.Parse(&cfgENV)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println("********3")
 	databaseDsn := cfgENV.Address
 	if _, ok := os.LookupEnv("DATABASE_DSN"); !ok {
 		databaseDsn = *addressPtr
