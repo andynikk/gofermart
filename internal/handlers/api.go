7be97ec32b90e8de759634ac2bdfe6f1b49738a0
andrey.mikhailov <andynik76@yandex.ru>
2022-10-30 17:20:02 +0300

    Gofermart 67

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -140,12 +140,14 @@ func (srv *Server) apiUserOrdersPOST(w http.ResponseWriter, r *http.Request) {
 	numOrder, err := strconv.Atoi(string(respByte))
 	if err != nil || numOrder == 0 {
 		constants.Logger.ErrorLog(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
 	}
 
 	//TODO: Проверка на Луна
 	if !luhn.Valid(numOrder) {
 		constants.Logger.ErrorLog(err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
 		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
