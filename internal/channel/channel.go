74a862241ba225b5d6143185f9d7175338475534
andrey.mikhailov <andynik76@yandex.ru>
2022-11-02 21:43:08 +0300

    Gofermart 84

--- internal/channel/channel.go
+++ internal/channel/channel.go
@@ -1,22 +1,10 @@
 package channel
 
-//type FullScoringOrder struct {
-//	ScoringOrder   *ScoringOrder
-//	ResponseStatus constants.Answer
-//}
-
 type ScoringOrder struct {
 	Order   string  `json:"order"`
 	Status  string  `json:"status"`
 	Accrual float64 `json:"accrual"`
 }
-
-//func NewFullScoringService() *FullScoringOrder {
-//	return &FullScoringOrder{
-//		ScoringOrder:   new(ScoringOrder),
-//		ResponseStatus: constants.AnswerSuccessfully,
-//	}
-//}
 
 func NewScoringOrder() *ScoringOrder {
 	return &ScoringOrder{
